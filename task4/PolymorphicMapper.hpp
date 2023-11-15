568d29bcc51f293fec3347489f83bbd93009a923
jakmobius <jakmobius@gmail.com>
2023-11-15 16:35:59 +0000

    Complete task4

--- task4/PolymorphicMapper.hpp
+++ task4/PolymorphicMapper.hpp
@@ -2,11 +2,25 @@
 
 #include <optional>
 
-
-template <class From, auto target>
-struct Mapping;
+template <class From, auto target> struct Mapping {
+  using MappingFrom = From;
+  static constexpr auto mappingTarget = target;
+};
 
 template <class Base, class Target, class... Mappings>
-struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+struct PolymorphicMapper {};
+
+template <class Base, class Target> struct PolymorphicMapper<Base, Target> {
+  static std::optional<Target> map(const Base &) { return std::nullopt; }
+};
+
+template <class Base, class Target, class Head, class... Tail>
+struct PolymorphicMapper<Base, Target, Head, Tail...> {
+  static std::optional<Target> map(const Base &object) {
+    if (dynamic_cast<const typename Head::MappingFrom *>(&object)) {
+      return Target(Head::mappingTarget);
+    }
+
+    return PolymorphicMapper<Base, Target, Tail...>::map(object);
+  }
 };
